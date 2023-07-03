909cffdba79c204389cd67b466f89b397c2b82db
adenhidayatuloh <hidayatulohaden@gmail.com>
2023-07-03 14:24:47 +0700

    Pakar Fixes #9

---
+++ lib/views/MenuPakar/Hasil.dart
@@ -0,0 +1,215 @@
+// ignore_for_file: must_be_immutable
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:tugasakhir_aplikasi_kesehatan/views/MenuPakar/Beranda.dart';
+import 'package:tugasakhir_aplikasi_kesehatan/views/MenuPakar/SistemPakar.dart';
+
+import '../../widgets/AppBar.dart';
+
+double hasil() {
+  return totalsemua! / gejalaData.length;
+}
+
+class HasilDeteksi extends StatefulWidget {
+  String MenuId;
+  String judul;
+
+  HasilDeteksi(this.MenuId, this.judul);
+
+  @override
+  State<HasilDeteksi> createState() => _HasilDeteksiState();
+}
+
+class _HasilDeteksiState extends State<HasilDeteksi> {
+  List<DocumentSnapshot> penangananData = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData(widget.MenuId);
+  }
+
+  Future<void> fetchData(String menuId) async {
+    final QuerySnapshot snapshot = await FirebaseFirestore.instance
+        .collection("tambah_menu")
+        .doc(menuId)
+        .collection("penanganan")
+        .get();
+    setState(() {
+      penangananData = snapshot.docs;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(19, 103, 187, 1),
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(19, 103, 187, 1),
+        title: appBar(context),
+        centerTitle: true,
+      ),
+      body: penangananData.isEmpty
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : penanganan(),
+      floatingActionButton: SizedBox(
+        width: 130,
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => Beranda()));
+          },
+          style: ButtonStyle(
+              backgroundColor:
+                  MaterialStatePropertyAll(Color.fromRGBO(255, 237, 0, 1)),
+              foregroundColor:
+                  MaterialStatePropertyAll(Color.fromARGB(255, 2, 54, 97)),
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                      side:
+                          BorderSide(color: Color.fromARGB(255, 2, 54, 97))))),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [Icon(Icons.navigate_before), Text('Back')],
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+    );
+  }
+
+  Widget? penanganan() {
+    if (hasil() <= 0.5) {
+      return Solusi(solusi: penangananData[0]["Rendah"], judul: widget.judul);
+    } else {
+      return Solusi(solusi: penangananData[0]["Tinggi"], judul: widget.judul);
+    }
+  }
+}
+
+class Solusi extends StatelessWidget {
+  final solusi;
+  final judul;
+  const Solusi({super.key, required this.solusi, required this.judul});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+              padding: const EdgeInsets.only(
+                  top: 13, bottom: 13, right: 13, left: 30),
+              child: resiko(hasil())),
+          Expanded(
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20)),
+                color: Colors.white,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Center(
+                    child: Text(
+                      "Hasil dari " + judul + " adalah : ",
+                      style: TextStyle(
+                          fontSize: 17,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 20, right: 80, top: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Kemungkinan anda terkena " +
+                              (hasil() * 100).toStringAsFixed(1) +
+                              " %",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 15,
+                          ),
+                        ),
+                        CircularProgressIndicator(
+                          value: hasil(),
+                          valueColor: AlwaysStoppedAnimation<Color>(
+                              _warnaresiko(hasil())),
+                          backgroundColor: Colors.grey[300],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20.0, top: 10),
+                    child: Text(
+                      "Apa yang sebaiknya anda lakukan : ",
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                        itemCount: solusi.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                              leading: Icon(Icons.circle),
+                              title: Text(
+                                solusi[index],
+                                style: TextStyle(fontSize: 15),
+                              ));
+                        }),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+Color _warnaresiko(double tingkat_bahaya) {
+  if (tingkat_bahaya < 0.5) {
+    return Colors.green;
+  }
+  if (tingkat_bahaya < 0.8) {
+    return Colors.orange;
+  } else {
+    return Colors.red;
+  }
+}
+
+Text resiko(double tingkat_resiko) {
+  if (tingkat_resiko <= 0.5) {
+    return Text(
+      "Hasil : Anda tidak berpotensi",
+      style: TextStyle(
+          color: Colors.white, fontSize: 20, fontWeight: FontWeight.w500),
+    );
+  } else {
+    return Text(
+      "Hasil : Anda berpotensi",
+      style: TextStyle(
+          color: Colors.white, fontSize: 20, fontWeight: FontWeight.w500),
+    );
+  }
+}
---
+++ lib/views/MenuPakar/Introduction.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+import 'package:tugasakhir_aplikasi_kesehatan/views/MenuPakar/SistemPakar.dart';
+
+class Introduction extends StatelessWidget {
+  final Menuid;
+  final Judul;
+  final fotoAhli;
+  final namaAhli;
+
+  const Introduction(
+      {super.key,
+      required this.Menuid,
+      required this.Judul,
+      required this.fotoAhli,
+      required this.namaAhli});
+
+  @override
+  Widget build(BuildContext context) {
+    return IntroductionScreen(
+      globalBackgroundColor: Colors.white,
+      pages: [
+        PageViewModel(
+            titleWidget: Text(
+              "Jawab Pertanyaan",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            body:
+                "Silahkan jawab pertanyaan dengan memilih opsi sesuai dengan keadaan anda sekarang",
+            image: Padding(
+              padding: const EdgeInsets.only(top: 40),
+              child: Image.asset(
+                'assets/images/Pertanyaan.png',
+              ),
+            )),
+        PageViewModel(
+            titleWidget: Text(
+              "Lihat Hasil",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            body:
+                "Jika sudah selesai menjawab pertanyaan, hasil akan muncul sesuai rekomendasi ahli",
+            image: Padding(
+              padding: const EdgeInsets.only(top: 40),
+              child: Image.asset(
+                'assets/images/Hasil.png',
+              ),
+            )),
+        PageViewModel(
+            titleWidget: Text(
+              "Ahli : $namaAhli ",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            body:
+                "Semua pertanyaan dan hasil merupakan apa yang di rekomendasikan oleh ahli yang bersangkutan",
+            image: Container(
+                height: 400,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    image: DecorationImage(
+                        image: NetworkImage(fotoAhli), fit: BoxFit.cover)))),
+      ],
+      onDone: () {
+        Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) => MenuPakar(Menuid, Judul)));
+      },
+      onSkip: () {
+        Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) => MenuPakar(Menuid, Judul)));
+      },
+      showSkipButton: true,
+      skip: Text(
+        "Skip",
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+      ),
+      next: Icon(Icons.arrow_forward),
+      done: Text("Lanjut",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+      dotsDecorator: DotsDecorator(
+        size: Size.square(10),
+        activeSize: Size(20, 10),
+        activeColor: Colors.black,
+        spacing: EdgeInsets.symmetric(horizontal: 3.0),
+      ),
+    );
+  }
+}
