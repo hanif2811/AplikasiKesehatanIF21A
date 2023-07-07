b77c799dfdf2ec21ed729d341fe1bc5bdec58c5e
Khead2 <ha149366@gmail.com>
2023-07-07 23:45:54 +0700

    tips and trick Fixes #58

--- lib/views/Admin/AdminBeranda.dart
+++ lib/views/Admin/AdminBeranda.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:tugasakhir_aplikasi_kesehatan/views/Admin/Admin_InsertRead/TambahIklan.dart';
 import 'package:tugasakhir_aplikasi_kesehatan/views/Admin/Admin_InsertRead/TambahMenu.dart';
+import 'package:tugasakhir_aplikasi_kesehatan/views/Admin/Admin_InsertRead/Tipstrik.dart';
 import 'package:tugasakhir_aplikasi_kesehatan/views/Admin/Admin_UpdateDelete/UpdateMenu.dart';
 
 class AdminBeranda extends StatefulWidget {
@@ -49,6 +50,14 @@ class _AdminBerandaState extends State<AdminBeranda> {
                           MaterialPageRoute(builder: (context) => Iklan()));
                     },
                     child: Text("Edit Iklan"))),
+            SizedBox(
+                width: 300,
+                child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => tipsTrik()));
+                    },
+                    child: Text("Edit Tips & Trik"))),
           ],
         ),
       ),
